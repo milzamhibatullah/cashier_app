0dd5e5eecc5f9900e017ef4031e97cfbb25b2347
email.milzam <email.milzam@gmail.com>
2024-11-07 09:04:29 +0000

    add: images and title on splash screen

---
+++ lib/core/utils/assets.manager.dart
@@ -0,0 +1,8 @@
+
+class AssetsManger {
+  static const String _assets = 'assets';
+  static const String _imageTypes = 'images';
+
+  ///images
+  static const String cashierImages = '$_assets/$_imageTypes/cashier.png';
+}
--- lib/features/splash/presentation/pages/splash.page.dart
+++ lib/features/splash/presentation/pages/splash.page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:myapp/core/utils/assets.manager.dart';
 
 ///halaman splash
 class SplashPage extends StatelessWidget {
@@ -6,6 +8,25 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: SizedBox(
+        width: 1.sw,
+        height: 1.sh,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AssetsManger.cashierImages,
+              height: 200.h,
+              width: 200.h,
+            ),
+            SizedBox(
+              height: 40.h,
+            ),
+            Text('Lorem Ipsum Dolor Imit')
+          ],
+        ),
+      ),
     );
   }
 }
